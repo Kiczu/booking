12af5792f6c7cc8f94dd9eef0ea3e775e4f9b6ef
Kiczu <adrian.mamyto@gmail.com>
2024-08-23 19:16:32 +0200

    test2

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 const asd = () => {};
+const as23d = () => {};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
