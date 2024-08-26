cd29fc532b88f833a27e9027d0a320642bc3fa6f
mkokosinski <mkokosinski91@gmail.com>
2024-08-26 16:08:53 +0200

    code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+console.log();
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    < >
-    </>
+    <App />
   </StrictMode>,
 )
