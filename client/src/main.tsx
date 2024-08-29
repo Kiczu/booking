577470f0e560401e6fb206a5a095e416f68f4c7a
Kiczu <adrian.mamyto@gmail.com>
2024-08-29 20:08:32 +0200

    build: add errorPage component to router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { paths } from "./paths";
 import Layout from "./Layout/Layout";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: paths.HOME,
     element: <Layout />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
